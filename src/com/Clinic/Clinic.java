f8cdbc542c6b87940bb72f3005a92b75cece604a
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-11-24 14:05:04 -0800

    Can read the file

--- src/com/Clinic/Clinic.java
+++ src/com/Clinic/Clinic.java
@@ -10,30 +10,100 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//https://stackoverflow.com/questions/37758566/when-to-use-a-string-with-filename-or-file-as-argument-to-constructor
+
 public class Clinic {
 
-    //instance variables
+    //***********************instance variables*****************
     File patientFile;
     int day;
+    Scanner fileScan = null;
 
-    //constructors
+    //*************************constructors*********************
     public Clinic(File file) {
         this.patientFile = file;
         //double health = this.getHealth();
     }
 
-
-    //methods
-
-    String nextday(File f) throws FileNotFoundException{
-        return null;
+    public Clinic (String filename) {
+        this(new File(filename));
     }
 
-    String nextday(String fileName) throws FileNotFoundException {
-        return null;
+
+    //**********************methods********************
+    public String nextDay(File f) throws FileNotFoundException{
+        //setup the variables
+        String[] tokens = null;
+        String[] petNames = new String[100];
+        Object[] petTypes = new Object[100];
+        int[] uniqueAtt = new int[100];
+        int[] times = new int[100];
+        int index = 0;
+        //setup a try block to make sure the read is handled
+        try {
+            fileScan = new Scanner(f);
+            String line = null;
+            //loop through the file to read the data
+            while (fileScan.hasNextLine()) {
+                line = fileScan.nextLine();
+                tokens = line.split(",");
+                petNames[index] = tokens[0];
+                petTypes[index] = tokens[1];
+                uniqueAtt[index] = Integer.parseInt(tokens[2]);
+                times[index] = Integer.parseInt(tokens[3]);
+                index++;
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            return null;
+        }
     }
 
-    boolean addToFile(String patientInfo){
+
+    /**
+     * Takes in a string with the file name and reads the files
+     * @param fileName
+     * @return
+     * @throws FileNotFoundException
+     */
+    public String nextDay(String fileName) throws FileNotFoundException {
+        //setup the variables
+        File fileIn = new File(fileName);
+        String[] tokens = null;
+        String[] petNames = new String[100];
+        Object[] petTypes = new Object[100];
+        int[] uniqueAtt = new int[100];
+        int[] times = new int[100];
+        int index = 0;
+
+        //setup a try block to make sure the read is handled
+        try {
+            fileScan = new Scanner(fileIn);
+            String line = null;
+            //loop through the file to read the data
+            while (fileScan.hasNextLine()) {
+                line = fileScan.nextLine();
+                tokens = line.split(",");
+                petNames[index] = tokens[0];
+                petTypes[index] = tokens[1];
+                uniqueAtt[index] = Integer.parseInt(tokens[2]);
+                times[index] = Integer.parseInt(tokens[3]);
+                index++;
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            return null;
+        }
+    }
+
+    public boolean addToFile(String patientInfo){
         return false;
     }
 
