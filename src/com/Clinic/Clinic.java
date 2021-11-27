eee3dc9a1e52e799e0fcb2f278ab67d33324d191
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-11-27 00:01:14 -0500

    Continued working nextday

--- src/com/Clinic/Clinic.java
+++ src/com/Clinic/Clinic.java
@@ -1,16 +1,9 @@
 package com.Clinic;
 
 
-import com.Cat.Cat;
-import com.Dog.Dog;
-import com.InvalidPetException.InvalidPetException;
-import com.Pet.Pet;
-
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Scanner;
 
 
 //https://stackoverflow.com/questions/37758566/when-to-use-a-string-with-filename-or-file-as-argument-to-constructor
@@ -21,6 +14,7 @@ public class Clinic{
     File patientFile;
     int day;
     Scanner fileScan = null;
+    Scanner inputs = null;
     private Object InvalidPetException;
 
     //*************************constructors*********************
@@ -35,11 +29,75 @@ public class Clinic{
 
 
     //**********************methods********************
+    /**
+     * Takes in a string with the file name and reads the files
+     * @param fileName
+     * @return
+     * @throws FileNotFoundException
+     */
+    public String nextDay(String fileName) throws FileNotFoundException {
+        //setup the variables
+        File fileIn = new File(fileName);
+        String[] tokens;
+        String[] petNames = new String[100];
+        Object[] petTypes = new Object[100];
+        int[] uniqueAtt = new int[100];
+        int[] times = new int[100];
+        int index = 0;
+        int userHealth;
+        double userPain;
+
+        //setup a try block to make sure the read is handled
+        try {
+            fileScan = new Scanner(fileIn);
+            inputs = new Scanner(System.in);
+            String line;
+            //loop through the file to read the data
+            while (fileScan.hasNextLine()) {
+                line = fileScan.nextLine();
+                tokens = line.split(",");
+                petNames[index] = tokens[0];
+                //may need to read this as strings
+                petTypes[index] = tokens[1];
+                uniqueAtt[index] = Integer.parseInt(tokens[2]);
+                times[index] = Integer.parseInt(tokens[3]);
+                index++;
+            }
+
+            //do some stuff with the data
+            //may need to make this loop
+            int j = 0;
+            System.out.println("Consultation for " + petNames[j] + " the " + petTypes[j] + " at " + times[j] +".\nWhat is the health of " + petNames[j] + "?\n");
+            if (petTypes[j].equals("Cat") | petTypes[j].equals("Dog")) {
+                //will need to setup an exception controlled loop to handled non int inputs
+                userHealth = inputs.nextInt();
+                System.out.println("On a scale of 1 to 10, how much pain is " + petNames[j] + "in right now?\n");
+                userPain = inputs.nextDouble();
+
+            } else {
+                throw (Throwable) InvalidPetException;
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            return null;
+        }
+    }
+
+
+
+
+
+
+
+
     public String nextDay(File f) throws FileNotFoundException{
         //setup the variables
         String[] tokens = null;
         String[] petNames = new String[100];
-        Object[] petTypes = new Object[100];
+        String[] petTypes = new String[100];
         int[] uniqueAtt = new int[100];
         int[] times = new int[100];
         int index = 0;
@@ -61,7 +119,9 @@ public class Clinic{
             //may need to make this loop
             int j = 0;
             System.out.println("Consultation for " + petNames[j] + " the " + petTypes[j] + " at " + times[j] +".\nWhat is the health of " + petNames[j] + "?\n");
-            if (petTypes[j] instanceof Cat | petTypes[j] instanceof Dog) {
+            if (petTypes[j].equals("Cat") | petTypes[j].equals("Dog")) {
+
+            } else {
                 throw (Throwable) InvalidPetException;
             }
         }
@@ -74,54 +134,7 @@ public class Clinic{
     }
 
 
-    /**
-     * Takes in a string with the file name and reads the files
-     * @param fileName
-     * @return
-     * @throws FileNotFoundException
-     */
-    public String nextDay(String fileName) throws FileNotFoundException {
-        //setup the variables
-        File fileIn = new File(fileName);
-        String[] tokens = null;
-        String[] petNames = new String[100];
-        Object[] petTypes = new Object[100];
-        int[] uniqueAtt = new int[100];
-        int[] times = new int[100];
-        int index = 0;
 
-        //setup a try block to make sure the read is handled
-        try {
-            fileScan = new Scanner(fileIn);
-            String line = null;
-            //loop through the file to read the data
-            while (fileScan.hasNextLine()) {
-                line = fileScan.nextLine();
-                tokens = line.split(",");
-                petNames[index] = tokens[0];
-                //may need to read this as strings
-                petTypes[index] = tokens[1];
-                uniqueAtt[index] = Integer.parseInt(tokens[2]);
-                times[index] = Integer.parseInt(tokens[3]);
-                index++;
-            }
-
-            //do some stuff with the data
-            //may need to make this loop
-            int j = 0;
-            System.out.println("Consultation for " + petNames[j] + " the " + petTypes[j] + " at " + times[j] +".\nWhat is the health of " + petNames[j] + "?\n");
-            if (petTypes[j] instanceof Cat | petTypes[j] instanceof Dog) {
-                throw (Throwable) InvalidPetException;
-            }
-
-        }
-        catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        finally {
-            return null;
-        }
-    }
 
     public boolean addToFile(String patientInfo){
         return false;
